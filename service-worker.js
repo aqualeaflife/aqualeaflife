6983dde2e976be885f86e8bacceb17d243ab9458
aqualeaflife <aquaticleaflife@gmail.com>
2026-08-13 10:44:03 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='aqualeaflife-v051';
+const CACHE='aqualeaflife-v052';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
